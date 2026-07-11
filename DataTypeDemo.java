8acd0201783094d6938f7a43623ad92962b2ec1c
Ritik Kumar Gupta <ritikgupta8170@gmail.com>
2026-07-11 11:30:19 +0530

    Added Data Type Demonstration in Java

---
+++ DataTypeDemo.java
@@ -0,0 +1,17 @@
+public class DataTypeDemo {
+
+    public static void main(String[] args) {
+        
+        int age = 20;
+        float height = 5.8f;
+        double salary = 55000.75;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Integer Value (age):" + age);
+        System.out.println("Float Value (height): " + height);
+        System.out.println("Double Value(salary): " + salary);
+        System.out.println("Character value (grade):" + grade);
+        System.out.println("Boolean Value (isStudent): "  + isStudent);
+    }
+}
