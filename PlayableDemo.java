ea35e59758cb0aa5dbcaccd60f2696ddfdb73c57
Ritik Kumar Gupta <ritikgupta8170@gmail.com>
2026-07-18 21:01:51 +0530

    Added Interface Implementation Example in Java

---
+++ PlayableDemo.java
@@ -0,0 +1,25 @@
+interface Playable {
+    void play();
+}
+
+class Guitar implements Playable {
+    public void play() {
+        System.out.println("Playing Guitar");
+    }
+}
+
+class Piano implements Playable {
+    public void play() {
+        System.out.println("Playing Piano");
+    }
+}
+
+public class PlayableDemo {
+    public static void main(String[] args) {
+        Playable guitar = new Guitar();
+        Playable piano = new Piano();
+
+        guitar.play();
+        piano.play();
+    }
+}
